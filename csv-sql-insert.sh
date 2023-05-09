6999db57b84d485f657c4f6767a4a2c34be4d7c9
RockkLee <sam880103@gmail.com>
2023-05-09 23:20:10 +0800

    add new shellscript (csv-to-select-sql, csv-to-insert-sql)

--- csv-sql-insert.sh
+++ csv-sql-insert.sh
@@ -5,7 +5,6 @@ op=$(echo "$fname" | cut -d"." -f 1)
 tmpname="${op}_temp.csv"
 sed 's/\s*,*\s*$//g' "$fname" > "$tmpname"
 opfile="$op.sql"
-opfile2="${op}_A.sql"
 op="$op"
 columns=$(head --lines=1 "$tmpname" | sed 's/,/,/g' | tr -d "\r\n")
 columns="$columns"
@@ -16,14 +15,5 @@ values=$(echo $l | sed 's/,/\",\"/g' | tr -d "\r\n")
 values="\"$values\""
 echo "INSERT INTO $op($columns) VALUES ($values);"
 done > "$opfile"
-
-echo "SELECT * FROM $op WHERE ($columns) IN (" > "$opfile2"
-# while-loop
-tail --lines=+2 "$tmpname" | while read l ; do
-values=$(echo $l | sed 's/,/\",\"/g' | tr -d "\r\n")
-values="\"$values\""
-echo "($values),"
-done >> "$opfile2"
-echo ");" >> "$opfile2"
 
 rm "$tmpname"
--- csv-sql-select.sh
+++ csv-sql-select.sh
@@ -4,18 +4,10 @@ fname="$1"
 op=$(echo "$fname" | cut -d"." -f 1)
 tmpname="${op}_temp.csv"
 sed 's/\s*,*\s*$//g' "$fname" > "$tmpname"
-opfile="$op.sql"
 opfile2="${op}_A.sql"
 op="$op"
 columns=$(head --lines=1 "$tmpname" | sed 's/,/,/g' | tr -d "\r\n")
 columns="$columns"
-
-# while-loop
-tail --lines=+2 "$tmpname" | while read l ; do
-values=$(echo $l | sed 's/,/\",\"/g' | tr -d "\r\n")
-values="\"$values\""
-echo "INSERT INTO $op($columns) VALUES ($values);"
-done > "$opfile"
 
 echo "SELECT * FROM $op WHERE ($columns) IN (" > "$opfile2"
 # while-loop
